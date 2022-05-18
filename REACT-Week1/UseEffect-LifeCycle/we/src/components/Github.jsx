bb48f4926b3a291e7784e2ca9e2dfce29b146808
Prashant-Bhatiya <developer.bhatiya@gmail.com>
2022-05-18 20:06:24 +0530

    Created Github Search Bar

---
+++ REACT-Week1/UseEffect-LifeCycle/we/src/components/Github.jsx
@@ -0,0 +1,15 @@
+import React , {useEffect} from "react";
+
+function Github() {
+  useEffect(() => {
+    <h1>New GitHub</h1>;
+
+    return () => {
+      
+    };
+  }, []);
+
+  return <div></div>;
+}
+
+export default Github;

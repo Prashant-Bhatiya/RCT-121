d7df591174c9890d11d42ec6670b1ef3392c817e
Prashant-Bhatiya <developer.bhatiya@gmail.com>
2022-05-19 18:48:25 +0530

    Created Timer

--- REACT-Week1/STATE-MANAGEMENT/GOOGLE-TIMER/timer_stopwatch/src/components/Timer.jsx
+++ REACT-Week1/STATE-MANAGEMENT/GOOGLE-TIMER/timer_stopwatch/src/components/Timer.jsx
@@ -4,31 +4,38 @@ import styles from "./Timer.module.css";
 function Timer() {
   const [second, setSecond] = useState(0);
   const [minutes, setMinutes] = useState(0);
+  const [stop, setStop] = useState(true);
 
   var timer;
 
   useEffect(() => {
-    timer = setInterval(() => {
-      setSecond(second + 1);
+    if (stop === true) {
+      timer = setInterval(() => {
+        setSecond(second + 1);
 
-      if (second === 59) {
-        setMinutes(minutes + 1);
-        setSecond(0);
-      }
-    }, 1000);
+        if (second === 59) {
+          setMinutes(minutes + 1);
+          setSecond(0);
+        }
+      }, 1000);
 
-    return () => {
-      console.log("Nothing !");
-      clearInterval(timer);
-    };
+      return () => {
+        clearInterval(timer);
+      };
+    }
   });
 
-  const handleRestart = ()=>{
-      setSecond(0);
-      setMinutes(0);
+  const handleRestart = () => {
+    setSecond(0);
+    setMinutes(0);
   };
-  const handleStop = ()=>{
-      clearInterval(timer)
+  const handleStop = (e) => {
+    {
+      e.target.innerText === "Stop" ? clearInterval(timer) : null;
+    }
+    console.log(e.target.innerText);
+    console.log(stop);
+    setStop(!stop);
   };
 
   return (
@@ -40,9 +47,13 @@ function Timer() {
             {minutes < 10 ? "0" + minutes : minutes} :{" "}
             {second < 10 ? "0" + second : second}
           </h1>
- 
-          <button className={styles.Restart} onClick={handleRestart}>Restart</button>
-          <button className={styles.Stop} onClick={handleStop}>Stop</button>
+
+          <button className={styles.Restart} onClick={handleRestart}>
+            Restart
+          </button>
+          <button className={styles.Stop} onClick={handleStop}>
+            {stop ? "Stop" : "Start"}
+          </button>
         </div>
       </div>
     </div>

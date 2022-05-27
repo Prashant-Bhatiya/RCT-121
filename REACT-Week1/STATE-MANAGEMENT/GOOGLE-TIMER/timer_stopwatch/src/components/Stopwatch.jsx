9173e0f69a043136779f97de55867ae7d5131117
Prashant-Bhatiya <developer.bhatiya@gmail.com>
2022-05-27 19:59:17 +0530

    Completed Stopwatch Assignment

--- REACT-Week1/STATE-MANAGEMENT/GOOGLE-TIMER/timer_stopwatch/src/components/Stopwatch.jsx
+++ REACT-Week1/STATE-MANAGEMENT/GOOGLE-TIMER/timer_stopwatch/src/components/Stopwatch.jsx
@@ -1,75 +1,64 @@
-// import React, { useState, useEffect } from "react";
-// import styles from "./Timer.module.css";
+import React, { useState, useEffect } from "react";
+import styles from "./Timer.module.css";
 
-// function Timer() {
-//   const [second, setSecond] = useState(0);
-//   const [minutes, setMinutes] = useState(0);
-//   const [stop, setStop] = useState(true);
+function Stopwatch() {
+  const [minutes, setMinutes] = useState(0);
+  const [second, setSecond] = useState(0);
+  const [milisecond, setMilsecond] = useState(0);
+  const [stop, setStop] = useState(false);
 
-//   var timer;
+  var timer;
 
-//   useEffect(() => {
-//     if (stop === true) {
-//       timer = setInterval(() => {
-//         setSecond(second + 1);
+  useEffect(() => {
+    if (stop === true) {
+      if (second === 59) {
+        setMinutes(minutes + 1);
+        setSecond(0);
+      }
+      if (milisecond === 100) {
+        setSecond(second + 1);
+        setMilsecond(0);
+      }
+      timer = setInterval(() => {
+        setMilsecond(milisecond + 1);
+      }, 10);
 
-//         if (second === 59) {
-//           setMinutes(minutes + 1);
-//           setSecond(0);
-//         }
-//       }, 1000);
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  });
 
-//       return () => {
-//         clearInterval(timer);
-//       };
-//     }
-//   });
+  const handleRestart = () => {
+    clearInterval(timer);
+    setMilsecond(0)
+    setSecond(0)
+    setStop(false)
+  };
 
-//   const handleRestart = () => {
-//     setSecond(0);
-//     setMinutes(0);
-//   };
-//   const handleStop = (e) => {
-//     {
-//       e.target.innerText === "Stop" ? clearInterval(timer) : null;
-//     }
-//     // console.log(e.target.innerText);
-//     // console.log(stop);
-//     setStop(!stop);
-//   };
+  const handleStop = (e) => {
+// /    console.log(e.target.innerText)
+    {
+      e.target.innerText === "Stop" ? clearInterval(timer) : null;
+    }
+    setStop(!stop);
+  };
 
-//   return (
-//     <div className={styles.Timer}>
-//       <div className={styles.container}>
-//         <div className={styles.TimerContainer}>
-//           <h1>Timer</h1>
-//           <h1>
-//             {minutes < 10 ? "0" + minutes : minutes} :{" "}
-//             {second < 10 ? "0" + second : second}
-//           </h1>
-
-//           <button className={styles.Restart} onClick={handleRestart}>
-//             Restart
-//           </button>
-//           <button className={styles.Stop} onClick={handleStop}>
-//             {stop ? "Stop" : "Start"}
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Timer;
-
-import React, { Component } from 'react'
-
-export class Stopwatch extends Component {
-  render() {
-    return (
-      <div>Stopwatch</div>
-    )
-  }
+  return (
+    <div className={styles.Timer}>
+      <div className={styles.container}>
+        <div className={styles.TimerContainer}>
+          <h1>Stopwatch</h1>
+          <h1>
+            {second < 10 ? "0" + second : second}{"s"}
+            {" : "}
+            {milisecond < 10 ? "0" + milisecond : milisecond}
+          </h1>
+          <button className={styles.Restart} onClick={handleRestart} >Reset</button>
+          <button className={styles.Stop} onClick={handleStop}>{stop ? "Stop" : "Start"}</button>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Stopwatch
+export default Stopwatch;

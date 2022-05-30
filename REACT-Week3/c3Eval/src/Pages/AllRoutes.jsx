f1a1be5346a82389030214a77737ddfbcbacda60
Prashant-Bhatiya <developer.bhatiya@gmail.com>
2022-05-30 16:21:58 +0530

    Created Two Coponents with Routing !

--- REACT-Week3/c3Eval/src/Pages/AllRoutes.jsx
+++ REACT-Week3/c3Eval/src/Pages/AllRoutes.jsx
@@ -1,10 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Route, Routes } from "react-router";
 import { Link } from "react-router-dom";
 import RegisterPageOne from "./RegisterPageOne";
 import RegisterPageTwo from "./RegisterPageTwo";
 
 function AllRoutes() {
+
+    const [para,setPara] = useState(false);
+
+    // console.log(para)
+    // console.log(setPara)
+    const HandleHomePagetwo = () => {
+        return <Navigate to="/register/two" /> 
+    };
+    
+    {para ? HandleHomePagetwo() : null}
+
   const HandleHomePage = () => {
       return <Navigate to={"/register/one"} />
   };
@@ -29,7 +40,7 @@ function AllRoutes() {
 
       <Routes>
         <Route path="/" element={<HandleHomePage />}></Route>
-        <Route path="/register/one" element={<RegisterPageOne users={users} />}></Route>
+        <Route path="/register/one" element={<RegisterPageOne users={users} para={para} setPara={setPara}/>}></Route>
         <Route path="/register/two" element={<RegisterPageTwo />}></Route>
       </Routes>
     </>
--- REACT-Week3/c3Eval/src/Pages/RegisterPageOne.jsx
+++ REACT-Week3/c3Eval/src/Pages/RegisterPageOne.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router";
-import { Link } from "react-router-dom";
 
-function RegisterPageOne({ users }) {
+function RegisterPageOne({ users , para , setPara}) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [verfiy, setVerify] = useState(false);
 
   const handleChange = (e) => {
     if (e.target.name === "Name") {
@@ -20,22 +18,16 @@ function RegisterPageOne({ users }) {
     if (email !== "" && name !== "") {
       users.map((ele) => {
         if (name === ele.name && email === ele.email) {
-          setVerify(true);
-          console.log("True as Fuck");
+          setPara(true)
         }
       });
     }
   };
-  VerifyUsers()
   return (
     <div>
       <input name="Name" placeholder="Enter Name" onChange={handleChange} />
       <input name="Email" placeholder="Enter Email" onChange={handleChange} />
-      {verfiy
-        ? () => {
-            return <button>Next</button>;
-          }
-        : null}
+      <button onClick={VerifyUsers}>Next</button>
     </div>
   );
 }

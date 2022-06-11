db4b7b68e62855cd37122019dfb72ebdcced2bb9
Prashant-Bhatiya <developer.bhatiya@gmail.com>
2022-06-11 14:39:49 +0530

    Pagination and Sorting Done

--- REACT-Week1/STATE-MANAGEMENT/RESTAURANT/restaurant/src/App.jsx
+++ REACT-Week1/STATE-MANAGEMENT/RESTAURANT/restaurant/src/App.jsx
@@ -7,9 +7,11 @@ function App() {
 
   return (
     <div>
-      <RestaurantForm />
-      <br />
-      <br />
+      <h1 style={{
+        border:"1px solid transparent",
+        width:"25%",
+        margin:"auto"
+      }}>Restaurant Details</h1>
       <RestaurantDetails />
     </div>
   );
--- REACT-Week1/STATE-MANAGEMENT/RESTAURANT/restaurant/src/components/RestaurantDetails.jsx
+++ REACT-Week1/STATE-MANAGEMENT/RESTAURANT/restaurant/src/components/RestaurantDetails.jsx
@@ -4,12 +4,23 @@ import React, { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 
 export const RestaurantDetails = () => {
+  const [page, setPage] = useState(1);
   const [dataArr, setDatArr] = useState([]);
+  const [sortFunc, setSortFunc] = useState("asc");
   useEffect(() => {
-    axios
-      .get("http://localhost:8080/restaurant")
+    axios({
+      method: "get",
+      url: "http://localhost:8080/restaurant",
+      params: {
+        _page: page,
+        _limit: 5,
+        _sort: "rating",
+        _order: sortFunc,
+      },
+    })
       .then((res) => {
         setDatArr(res.data);
+        // console.log(res.headers);
       })
       .catch((err) => {
         console.log(err);
@@ -18,11 +29,78 @@ export const RestaurantDetails = () => {
     return () => {
       console.log("second");
     };
-  }, []);
-
+  }, [page, sortFunc]);
   return (
     <>
       <Container>
+        <div
+          style={{
+            border: "1px solid transparent",
+            width: "25%",
+            margin: "auto",
+          }}
+        >
+          <button
+            style={{
+              fontSize: "15px",
+            }}
+            disabled={sortFunc === "asc"}
+            onClick={() => {
+              setSortFunc("asc");
+            }}
+          >
+            Sort by Ascending
+          </button>
+          <button
+            style={{
+              fontSize: "15px",
+            }}
+            disabled={sortFunc === "desc"}
+            onClick={() => {
+              setSortFunc("desc");
+            }}
+          >
+            Sort by Decending
+          </button>
+        </div>
+        <div
+          style={{
+            border: "1px solid transparent",
+            width: "10%",
+            margin: "auto",
+          }}
+        >
+          <button
+            style={{
+              fontSize: "18px",
+              border: "1px solid transparent",
+            }}
+            disabled={page === 1}
+            onClick={() => {
+              setPage(page - 1);
+            }}
+          >
+            Prev
+          </button>
+
+          <PaginationComponents
+            currentPage={page}
+            lastPage={5}
+            onPageChange={setPage}
+          />
+          <button
+            style={{
+              fontSize: "18px",
+              border: "1px solid transparent",
+            }}
+            onClick={() => {
+              setPage(page + 1);
+            }}
+            disabled={page === 5}
+          >
+            Next
+          </button>
+        </div>
         {dataArr.map((ch) => {
           return (
             <Box key={ch._id}>
@@ -32,5 +110,31 @@ export const RestaurantDetails = () => {
         })}
       </Container>
     </>
+  );
+};
+
+const PaginationComponents = ({ currentPage, lastPage, onPageChange }) => {
+  const arr = new Array(lastPage).fill(0);
+  return (
+    <div
+      key={currentPage}
+      style={{
+        marginLeft: "-25px",
+        border: "1px solid transparent",
+      }}
+    >
+      {arr.map((item, page) => (
+        <button
+          style={{
+            fontSize: "15px",
+            border: "1px solid transparent",
+          }}
+          disabled={page + 1 === currentPage}
+          onClick={() => onPageChange(page + 1)}
+        >
+          {page + 1}
+        </button>
+      ))}
+    </div>
   );
 };

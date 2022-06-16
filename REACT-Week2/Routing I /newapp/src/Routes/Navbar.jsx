418798d68e771281f0a31e2129e1977042c6b14c
Prashant-Bhatiya <developer.bhatiya@gmail.com>
2022-06-16 12:36:09 +0530

    Completed USers Routes and individual USers Data

--- REACT-Week2/Routing I /newapp/src/Routes/Navbar.jsx
+++ REACT-Week2/Routing I /newapp/src/Routes/Navbar.jsx
@@ -1,16 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const activeStyle = { color: "red", textDecoration: "none" };
   return (
-    <nav style={{
-        display:"flex",
-        gap : "2rem",
-        justifyContent:"center"
-    }}>
-      <Link to={"/"}>Home</Link>{"  "} <Link to={"/about"}>About</Link>{" "}
-      <Link to={"/contact"}>Contact</Link>{" "}
-      <Link to={"/users"}>Users</Link>{" "}
+    <nav
+      style={{
+        display: "flex",
+        gap: "2rem",
+        justifyContent: "center",
+      }}
+    >
+      <NavLink
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        to={"/"}
+      >
+        Home
+      </NavLink>
+      {"  "}{" "}
+      <NavLink
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        to={"/about"}
+      >
+        About
+      </NavLink>{" "}
+      <NavLink
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        to={"/contact"}
+      >
+        Contact
+      </NavLink>{" "}
+      <NavLink
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+        to={"/users"}
+      >
+        Users
+      </NavLink>{" "}
     </nav>
   );
 };
